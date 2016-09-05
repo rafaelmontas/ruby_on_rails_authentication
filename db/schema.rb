8d2820c58d5566298fa90a438a239d6c8f862b7a
codecademy <ccuser@codecademy.com>
2016-09-05 11:58:28 -0400

    Working

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160904144134) do
+ActiveRecord::Schema.define(version: 20160905152833) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20160904144134) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
 end
